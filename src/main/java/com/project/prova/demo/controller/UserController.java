9fac63f720eb81db15994ffb93002757f27dfe8c
dzac <davide.zaccarella@nttdata.com>
2024-12-06 10:24:31 +0100

    first_commit

---
+++ src/main/java/com/project/prova/demo/controller/UserController.java
@@ -0,0 +1,49 @@
+package com.project.prova.demo.controller;
+
+import com.project.prova.demo.model.User;
+import com.project.prova.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    // Leggi tutti gli utenti
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    // Crea un nuovo utente
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    // Leggi un utente per ID
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable String id) {
+        return userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
+    }
+
+    // Aggiorna un utente
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable String id, @RequestBody User userDetails) {
+        User user = userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
+        user.setName(userDetails.getName());
+        user.setEmail(userDetails.getEmail());
+        return userRepository.save(user);
+    }
+
+    // Elimina un utente
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable String id) {
+        userRepository.deleteById(id);
+    }
+}
---
+++ src/main/java/com/project/prova/demo/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.project.prova.demo.repository;
+
+import com.project.prova.demo.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserRepository extends MongoRepository<User, String> {
+}
